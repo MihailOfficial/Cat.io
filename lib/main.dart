3476e19afad62695aa562f86c13eb3a95dab7a97
c_java <aurko.nandi@gmail.com>
2020-07-30 19:42:31 -0400

    added score text

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flame/components/animation_component.dart';
 import 'package:flame/components/component.dart';
 import 'package:flame/components/mixins/resizable.dart';
 import 'package:flame/components/parallax_component.dart';
+import 'package:flame/components/text_box_component.dart';
 import 'package:flame/components/text_component.dart';
 import 'package:flame/game/base_game.dart';
 import 'package:flame/palette.dart';
@@ -18,13 +19,15 @@ import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
-
+import "package:normal/normal.dart";
+import "package:flame/time.dart";
 
 const COLOR = const Color(0xff0000ff);
 const SIZE = 52.0;
 const GRAVITY = 700.0;
 const BOOST = -300;
-
+var score = 0;
+bool updateScore = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -39,7 +42,6 @@ class Bg extends Component with Resizable {
   @override
   void render(Canvas c) {
     c.drawRect(Rect.fromLTWH(0.0, 0.0, 50, 50), _paint);
-
   }
 
   @override
@@ -83,7 +85,7 @@ class Cat extends AnimationComponent with Resizable {
   void update(double t) {
     super.update(t);
     if (!frozen) {
-      this.y += speedY * t - GRAVITY * t * t / 2;
+      this.y += speedY * t; // - GRAVITY * t * t / 2
       this.speedY += GRAVITY * t;
       this.angle = velocity.angle();
       if (y > size.height || y < 0) {
@@ -108,7 +110,7 @@ double compy = 0.0;
 double printer = 0.0;
 
 class Coin extends AnimationComponent with Resizable {
-  double speedX = 2.0;
+  double speedX = 200.0;
   double posX, posY;
 
   Coin(double posX, double posY)
@@ -117,7 +119,6 @@ class Coin extends AnimationComponent with Resizable {
     this.anchor = Anchor.center;
     this.x = posX;
     this.y = posY;
-
   }
   reset() {
     this.x = size.width;
@@ -129,29 +130,42 @@ class Coin extends AnimationComponent with Resizable {
   void update(double t) {
     if (x < 0) {
       destroy();
-
-
     }
     double dist = sqrt((compy-y)*(compy-y) + (compx-x)*(compx-x));
 
     if (dist < 45) {
      this.x = -200000;
-      this.y = -200000;
-
+     this.y = -200000;
+     score++;
+     updateScore = true;
+     return;
     }
     super.update(t);
-    this.x -= speedX * 2;
+    this.x -= speedX * t;
   }
 }
-
 
 class MyGame extends BaseGame {
   var rng;
   Cat cat;
+  double timer;
+  List coinPatterns = [];
+  TextPainter textPainter;
+  Offset position;
 
-  var coinPattern = [[true, false, true],
-                     [false, true, false],
-                     [true, false, true]];
+  var coinPattern1 = [[1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 1]];
+  var coinPattern2 = [[1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]];
+  var coinPattern3 = [[0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]];
+  var coinPattern4 = [[1, 1, 1, 1, 1, 1]];
+  var coinPattern5 = [[0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0]];
 
   static List<ParallaxImage> images = [
 
@@ -163,18 +177,24 @@ class MyGame extends BaseGame {
   ];
   final parallaxComponent = ParallaxComponent(images,
       baseSpeed: const Offset(20, 0), layerDelta: const Offset(30, 0));
+
   MyGame(Size size) {
     add(parallaxComponent);
     add(cat = Cat());
     this.rng = new Random();
-
-    add(TextComponent('Score:')
-
-      ..anchor = Anchor.centerRight
-      ..x = size.width / 2
-      ..y = 32.0);
+    this.timer = Normal.quantile(rng.nextDouble(), mean: 2, variance: 0.5);
+    coinPatterns.add(coinPattern1);
+    coinPatterns.add(coinPattern2);
+    coinPatterns.add(coinPattern3);
+    coinPatterns.add(coinPattern4);
+    coinPatterns.add(coinPattern5);
+    textPainter = TextPainter(text: TextSpan(text: "Score: " + score.toString(), style: TextStyle(color: Colors.white, fontFamily: "pixelFont", fontSize: 25)), textDirection: TextDirection.ltr);
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    position = Offset(size.width / 2 - textPainter.width / 2, size.height * 0.05 - textPainter.height / 2);
   }
-
   @override
   void onTapDown(TapDownDetails details) {
     cat.onTap();
@@ -188,19 +208,39 @@ class MyGame extends BaseGame {
   void update(double t) {
     super.update(t);
 
+    timer -= t;
+    if (timer < 0) {
+      timer = Normal.quantile(rng.nextDouble(), mean: 0, variance: 3) + 6.0;
+      int pattern = rng.nextInt(coinPatterns.length);
+      print(pattern);
+      var coinPattern = coinPatterns[pattern];
+      double patternHeight = rng.nextDouble() * (size.height - 30.0 * coinPattern.length - 10.0);
 
-    if(rng.nextInt(1000) < (1000.0/(60.0 * 1.2))){
-      double height = rng.nextDouble() * (size.height - 30.0 * coinPattern.length);
-      for(var i = 0; i < coinPattern.length; i++){
-        for(var j = 0; j < coinPattern[i].length; j++){
-          if(coinPattern[j][i]){
-            add(new Coin(size.width + j * 30.0, height + i * 30.0));
-
+      for (var i = 0; i < coinPattern.length; i++) {
+        for (var j = 0; j < coinPattern[i].length; j++) {
+          if (coinPattern[i][j] == 1) {
+            add(new Coin(
+                size.width + j * 30.0 + 10, patternHeight + i * 30.0 + 10));
           }
         }
       }
     }
 
+    if(updateScore){
+      textPainter = TextPainter(text: TextSpan(text: "Score: " + score.toString(), style: TextStyle(color: Colors.white, fontFamily: "pixelFont", fontSize: 25)), textDirection: TextDirection.ltr);
+      textPainter.layout(
+        minWidth: 0,
+        maxWidth: size.width,
+      );
+      position = Offset(size.width / 2 - textPainter.width / 2, size.height * 0.05 - textPainter.height / 2);
+      updateScore = false;
+    }
+  }
+
+  @override
+  void render(Canvas c){
+    super.render(c);
+    textPainter.paint(c, position);
   }
 
 }
